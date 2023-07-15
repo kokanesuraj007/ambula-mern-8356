3f1288e4690f0eecc7b2782abccce2ddf65106a9
suraj sanjay kokane <108039887+kokanesuraj007@users.noreply.github.com>
2023-07-15 21:34:46 +0530

    add navbar icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Box, Flex, Link, useColorModeValue } from '@chakra-ui/react';
+import { AiFillHome, AiOutlineInfoCircle, AiOutlinePhone, AiOutlineCheckSquare } from 'react-icons/ai';
+import { BsFillBagFill } from 'react-icons/bs';
 
 const Navbar = () => {
   return (
@@ -16,30 +18,46 @@ const Navbar = () => {
           Ambula
         </Link>
         <Flex as="ul" listStyleType="none" alignItems="center" flexGrow="1" justifyContent="flex-end">
-          <NavItem to="/home">Home</NavItem>
-          <NavItem to="/about">About</NavItem>
-          <NavItem to="/contact">Contact</NavItem>
-          <NavItem to="/todo">Todo</NavItem>
-          <NavItem to="/product">Product</NavItem>
-          <NavItem to="/cart">Cart</NavItem>
+          <NavItem to="/" icon={<AiFillHome />} exact>
+            Home
+          </NavItem>
+          <NavItem to="/about" icon={<AiOutlineInfoCircle />}>
+            About
+          </NavItem>
+          <NavItem to="/contact" icon={<AiOutlinePhone />}>
+            Contact
+          </NavItem>
+          <NavItem to="/todo" icon={<AiOutlineCheckSquare />}>
+            Todo
+          </NavItem>
+          <NavItem to="/product" icon={<BsFillBagFill />}>
+            Product
+          </NavItem>
+          <NavItem to="/cart" icon={<BsFillBagFill />}>
+            Cart
+          </NavItem>
         </Flex>
       </Flex>
     </Box>
   );
 };
 
-const NavItem = ({ to, children }) => {
+const NavItem = ({ to, icon, children, ...rest }) => {
   return (
     <Box as="li" marginLeft="4">
       <Link
         as={NavLink}
         to={to}
+        display="flex"
+        alignItems="center"
         fontSize="20px"
         fontWeight="bold"
         _hover={{ textDecoration: 'none' }}
         activeClassName="active"
+        {...rest}
       >
-        {children}
+        <Box marginRight="4">{icon}</Box>
+        <span>{children}</span>
       </Link>
     </Box>
   );
